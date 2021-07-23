5ecc8df0e4d0cb3d7cdea1aa65b0cea31478c4c8
PdoubleU <piotrwitasik1988@gmail.com>
2021-07-23 07:46:02 +0200

    [pagination]: implement pagination using jw-pagination library 	- remove Pagination folder 	- set up pagination component in PostsContainer

--- src/components/PostsContainer/Pagination/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-   <div >
-   </div>
-</template>
-<style src="./style.sass" lang="sass"></style>
-<script src="./component.js" />
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import JwPagination from 'jw-vue-pagination'
+
+Vue.component('jw-pagination', JwPagination)
 
 Vue.config.productionTip = false
 
