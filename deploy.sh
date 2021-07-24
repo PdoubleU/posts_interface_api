14e2cd864155330e51f976491cae39f51aa81421
PdoubleU <piotrwitasik1988@gmail.com>
2021-07-24 08:33:34 +0200

    [gh setup]

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# abort on errorss
+et -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+cd -
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 const resolve = dir => path.join(__dirname, dir)
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/posts_interface_api/' : '/',
   configureWebpack: {
     resolve: {
       extensions: ['.js', '.vue', '.json'],
