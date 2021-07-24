86e71bd8215f975d116c842b7665b6bdc2b6b2e4
PdoubleU <piotrwitasik1988@gmail.com>
2021-07-24 12:32:31 +0200

    [components]: set up loading/error screen

--- src/components/PostsContainer/index.vue
+++ src/components/PostsContainer/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="posts-container">
     <ul class="posts-list">
+      <Loading :loading='isLoading' :error='isError'></Loading>
       <li v-bind:key="post.id" v-for="post in pageOfPosts">
         <PostBox v-bind:post='post'></PostBox>
       </li>
     </ul>
-    <div class="pagination-container">
+    <div class="pagination-container" v-if="!isLoading && !isError">
       <jw-pagination :items='allPosts' @changePage='onChangePage' :disableDefaultStyles='true' :maxPages='3' :labels='customLabels'></jw-pagination>
     </div>
   </div>
